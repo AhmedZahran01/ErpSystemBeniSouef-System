e0bac39feb0947a47d1ced2257a690621977d1bc
Ibrahem607 <ibrahemmalek607@gmail.com>
2025-09-15 05:08:08 +0300

    supplier invoice screen

---
+++ ErpSystemBeniSouef/Views/Pages/SupplierMainPage/SupplierInvoicesPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ErpSystemBeniSouef.Views.Pages.SupplierMainPage
+{
+    /// <summary>
+    /// Interaction logic for SupplierInvoicesPage.xaml
+    /// </summary>
+    public partial class SupplierInvoicesPage : Page
+    {
+        public SupplierInvoicesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
