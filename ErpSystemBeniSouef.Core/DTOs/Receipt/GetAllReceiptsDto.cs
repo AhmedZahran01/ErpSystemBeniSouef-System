532a18ce526c612bad7ec031ae9f28a14d2b21af
Adham <Adham@DESKTOP-NUQKQNJ>
2025-12-15 12:56:29 +0200

    Add GetAllReceiptsAsync Service In Receipts Section

---
+++ ErpSystemBeniSouef.Core/DTOs/Receipt/GetAllReceiptsDto.cs
@@ -0,0 +1,21 @@
+﻿namespace ErpSystemBeniSouef.Core.DTOs.Receipt;
+
+public class GetAllReceiptsDto
+{
+    public int MonthlyInstallmentId { get; set; }
+    public int CustomerNumber { get; set; }
+    public string CustomerName { get; set; } = string.Empty;    
+    public string MobileNumber { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string NationlNumber { get; set; } = string.Empty;
+    public decimal Deposite { get; set; }
+    public string CollectorName { get; set; } = string.Empty;
+    public string RepresentativeName { get; set; } = string.Empty;
+    public string AreaName { get; set; } = string.Empty;
+    public DateTime InvoiceDate { get; set; }
+    public DateTime MonthDate { get; set; }
+    public decimal TotalPrice { get; set; }
+
+    public List<InstallmentPlanDto> Plans { get; set; } = [];
+    public List<CustomerInvoiceItemDto> Items { get; set; } = [];
+}
---
+++ ErpSystemBeniSouef.Service/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using ErpSystemBeniSouef.Core.DTOs.Receipt;
+global using ErpSystemBeniSouef.Core;
+global using ErpSystemBeniSouef.Core.Contract;
+global using ErpSystemBeniSouef.Core.Entities.CustomerInvoices;
+global using ErpSystemBeniSouef.Core.DTOs.Reports;
+global using Microsoft.EntityFrameworkCore;
+global using ClosedXML.Excel;
