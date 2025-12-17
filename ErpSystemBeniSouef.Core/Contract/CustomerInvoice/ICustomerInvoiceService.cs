df3e8c4e59ad428463f89724bfc17a4b8b2d3e32
Adham <Adham@DESKTOP-NUQKQNJ>
2025-12-17 18:20:03 +0200

    Add UpdateInstallmentsAsync & GetMonthlyInstallmentsByInvoiceIdAsync Services in Customer Invoices Section

--- ErpSystemBeniSouef.Core/Contract/CustomerInvoice/ICustomerInvoiceService.cs
+++ ErpSystemBeniSouef.Core/Contract/CustomerInvoice/ICustomerInvoiceService.cs
@@ -1,22 +1,12 @@
-﻿using ErpSystemBeniSouef.Core.DTOs.CustomerInvoiceDtos.GetAllDetailsForCustomerInvoiceDtos;
-using ErpSystemBeniSouef.Core.DTOs.CustomerInvoiceDtos.Input;
-using ErpSystemBeniSouef.Core.DTOs.CustomerInvoiceDtos.output;
-using ErpSystemBeniSouef.Core.GenericResponseModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ErpSystemBeniSouef.Core.Contract.CustomerInvoice;
 
-namespace ErpSystemBeniSouef.Core.Contract.CustomerInvoice
+public interface ICustomerInvoiceService
 {
-    public interface ICustomerInvoiceService
-    {
-        Task<ServiceResponse<bool>> CreateCustomerInvoiceAsync(CreateCustomerInvoiceDTO dto);
-        Task<ServiceResponse<IReadOnlyList<ReturnCustomerInvoiceListDTO>>> GetAllCustomerInvoicesAsync();
-        Task<ServiceResponse<ReturnCustomerInvoiceDetailsDTO>> GetCustomerInvoiceByIdAsync(int invoiceId);
-        Task<ServiceResponse<bool>> DeleteCustomerAsync(int customerId);
-        Task<ServiceResponse<bool>> UpdateCustomerInvoiceAsync(int invoiceId, UpdateCustomerInvoiceDTO dto);
-
-    }
+    Task<ServiceResponse<bool>> CreateCustomerInvoiceAsync(CreateCustomerInvoiceDTO dto);
+    Task<ServiceResponse<IReadOnlyList<ReturnCustomerInvoiceListDTO>>> GetAllCustomerInvoicesAsync();
+    Task<ServiceResponse<ReturnCustomerInvoiceDetailsDTO>> GetCustomerInvoiceByIdAsync(int invoiceId);
+    Task<ServiceResponse<bool>> DeleteCustomerAsync(int customerId);
+    Task<ServiceResponse<bool>> UpdateCustomerInvoiceAsync(int invoiceId, UpdateCustomerInvoiceDTO dto);
+    Task<ServiceResponse<List<MonthlyInstallmentDto>>> GetMonthlyInstallmentsByInvoiceIdAsync(int customerInvoiceId);
+    Task<ServiceResponse<bool>> UpdateInstallmentsAsync(UpdateMonthlyInstallmentsDto dto);
 }
--- ErpSystemBeniSouef.Core/Entities/CustomerInvoices/CustomerInvoice.cs
+++ ErpSystemBeniSouef.Core/Entities/CustomerInvoices/CustomerInvoice.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ErpSystemBeniSouef.Core.Entities.CustomerInvoices;
 
-namespace ErpSystemBeniSouef.Core.Entities.CustomerInvoices
+public class CustomerInvoice:BaseEntity
 {
-    public class CustomerInvoice:BaseEntity
-    {
-        public int CustomerId { get; set; }
-        public Customer Customer { get; set; }
+    public int CustomerId { get; set; }
+    public Customer Customer { get; set; } = default!;
+    public DateTime InvoiceDate { get; set; }
+    public decimal TotalAmount { get; set; }
 
-        public DateTime InvoiceDate { get; set; }
-
-        public decimal TotalAmount { get; set; }
-
-        // Navigation
-        public ICollection<CustomerInvoiceItems>? Items { get; set; }
-        public ICollection<InstallmentPlan>? Installments { get; set; }
-    }
+    public ICollection<CustomerInvoiceItems>? Items { get; set; }
+    public ICollection<InstallmentPlan>? Installments { get; set; }
 }
